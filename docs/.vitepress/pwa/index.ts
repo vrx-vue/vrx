90590940d10423dc398ecd10ae09aea821adf4e3
whitekite <1075790909@qq.com>
2023-09-10 23:29:59 +0800

    docs: update pwa registerType

--- docs/.vitepress/pwa/index.ts
+++ docs/.vitepress/pwa/index.ts
@@ -2,7 +2,7 @@ import type { PwaOptions } from '@vite-pwa/vitepress'
 import { icons } from './icons'
 
 export const pwa: PwaOptions = {
-  registerType:'autoUpdate',
+  registerType: 'autoUpdate',
   manifest: {
     name: 'Vrx',
     short_name: 'Vrx',
