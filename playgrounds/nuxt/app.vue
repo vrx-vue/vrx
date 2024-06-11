3fa57609003501ef1ba2e6093ff832e49c093aea
whitekite <1075790909@qq.com>
2024-06-12 01:50:48 +0800

    chore(deps): update deps

--- playgrounds/nuxt/app.vue
+++ playgrounds/nuxt/app.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-  $useAsyncData()
+  const { data } = $useAsyncData(() => Promise.resolve('vrx/core'), { immediate: true })
 </script>
 <template>
-  <div>wwww</div>
+  <div>{{ data }}</div>
 </template>
